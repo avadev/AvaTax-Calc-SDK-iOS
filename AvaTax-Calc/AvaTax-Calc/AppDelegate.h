464d18d35cc44926de86a60de1afc52428a7f329
Rob Busack <rob.busack@gmail.com>
2013-12-16 13:38:03 -0800

    Initial commit.  Created an empty Xcode project.

---
+++ AvaTax-Calc/AvaTax-Calc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AvaTax-Calc
+//
+//  Created by Rob Busack on 12/16/13.
+//  Copyright (c) 2013 Avalara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AvaTax-Calc/AvaTax-Calc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AvaTax-Calc
+//
+//  Created by Rob Busack on 12/16/13.
+//  Copyright (c) 2013 Avalara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
