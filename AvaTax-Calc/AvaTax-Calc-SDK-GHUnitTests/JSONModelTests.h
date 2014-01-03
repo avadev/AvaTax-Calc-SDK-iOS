aeb8ad05bb0b18ccefe1cb4260b63e9ca13f5035
Rob Busack <rob.busack@gmail.com>
2014-01-03 14:37:24 -0800

    Adding a simple test.

---
+++ AvaTax-Calc/AvaTax-Calc-SDK-GHUnitTests/JSONModelTests.h
@@ -0,0 +1,15 @@
+//
+//  JSONModelTests.h
+//  AvaTax-Calc
+//
+//  Created by Rob Busack on 1/3/14.
+//  Copyright (c) 2014 Avalara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GHUnitIOS/GHUnit.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+@interface JSONModelTests : GHTestCase
+
+@end
---
+++ AvaTax-Calc/AvaTax-Calc-SDK-GHUnitTests/JSONModelTests.m
@@ -0,0 +1,29 @@
+//
+//  JSONModelTests.m
+//  AvaTax-Calc
+//
+//  Created by Rob Busack on 1/3/14.
+//  Copyright (c) 2014 Avalara. All rights reserved.
+//
+
+#import "JSONModelTests.h"
+#import "JSONModel.h"
+#import "GetTaxRequestBody.h"
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+@implementation JSONModelTests
+
+- (void)testJsonModeling {
+    
+    NSString* originalJson = @"{ \"DocDate\": \"2013-05-13\", \"CustomerCode\": \"CUST1\", \"DetailLevel\":\"Document\", \"Addresses\":[ { \"AddressCode\": \"1\", \"Line1\": \"100 Ravine Ln\", \"Line2\": \"\", \"City\": \"Bainbridge Island\", \"Region\": \"WA\", \"PostalCode\": \"98110\" }], \"Lines\":[ { \"LineNo\": \"1\", \"DestinationCode\": \"1\", \"OriginCode\": \"1\", \"Qty\": 1, \"Amount\": 100 }] }";
+    
+    JSONModelError* error = nil;
+    GetTaxRequestBody* jsonObject = [[GetTaxRequestBody alloc] initWithString:originalJson error:&error];
+    
+    GHAssertNil(error, @"");
+    NSString* outputJson = [jsonObject toJSONString];
+    
+    GHAssertTrue(outputJson.length > 0, @"");
+}
+
+@end
