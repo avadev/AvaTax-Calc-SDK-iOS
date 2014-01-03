9e1505998e855802fede2267592fd60e386460ad
Rob Busack <rob.busack@gmail.com>
2014-01-03 13:57:46 -0800

    Setting up a GHUnit test target that can be run.

---
+++ AvaTax-Calc/AvaTax-Calc-SDK-GHUnitTests/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AvaTax-Calc-SDK-GHUnitTests
+//
+//  Created by Rob Busack on 1/3/14.
+//  Copyright (c) 2014 Avalara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+    }
+}
