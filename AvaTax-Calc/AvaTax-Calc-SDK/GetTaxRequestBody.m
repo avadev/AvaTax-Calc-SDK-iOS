009dc2e413bc4cd23459204d5480366fd3246abc
Rob Busack <rob.busack@gmail.com>
2014-01-14 16:46:09 -0800

    Success!  At least one enum supported within the context of JSONModel.  Now I can do the rest.

--- AvaTax-Calc/AvaTax-Calc-SDK/GetTaxRequestBody.m
+++ AvaTax-Calc/AvaTax-Calc-SDK/GetTaxRequestBody.m
@@ -14,13 +14,45 @@
 +(BOOL)propertyIsIgnored:(NSString*)propertyName {
     if ([propertyName isEqualToString:@"Commit"]
         || [propertyName isEqualToString:@"Discount"]
-        || [propertyName isEqualToString:@"DocType"]
-        || [propertyName isEqualToString:@"DetailLevel"]) {
+        || [propertyName isEqualToString:@"DocType"]) {
         return YES;
     }
     
     return [super propertyIsIgnored:propertyName];
 }
+
+- (void)setDetailLevelWithNSString:(NSString*)detailLevelString {
+    if ([detailLevelString isEqualToString:@"Tax"]) {
+        self.DetailLevel = Tax;
+    } else if ([detailLevelString isEqualToString:@"Document"]) {
+        self.DetailLevel = Document;
+    } else if ([detailLevelString isEqualToString:@"Line"]) {
+        self.DetailLevel = Line;
+    } else if ([detailLevelString isEqualToString:@"Diagnostic"]) {
+        self.DetailLevel = Diagnostic;
+    }
+}
+
+- (id)JSONObjectForDetailLevel {
+    switch (self.DetailLevel) {
+        case Tax:
+            return @"Tax";
+            break;
+        case Document:
+            return @"Document";
+            break;
+        case Line:
+            return @"Line";
+            break;
+        case Diagnostic:
+            return @"Diagnostic";
+            break;
+        default:
+            return nil;
+            break;
+    }
+}
+
 
 @end
 
