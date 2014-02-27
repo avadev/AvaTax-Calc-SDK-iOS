3d8f56324d639558ff5d7fb9bc2e288f3253b772
Rob Busack <rob.busack@gmail.com>
2014-02-27 09:36:59 -0800

    Carrier route is not really an enumeration, only the first letter is, the rest is a string of numbers, so the whole thing has to be treated as a string.

--- AvaTax-Calc/AvaTax-Calc-SDK/AvaTaxValidatedAddress.h
+++ AvaTax-Calc/AvaTax-Calc-SDK/AvaTaxValidatedAddress.h
@@ -17,21 +17,13 @@ typedef enum {
     AddressType_S
 } AvaTaxAddressType;
 
-typedef enum {
-    CarrierRoute_B,
-    CarrierRoute_C,
-    CarrierRoute_G,
-    CarrierRoute_H,
-    CarrierRoute_R
-} AvaTaxCarrierRoute;
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 @interface AvaTaxValidatedAddress : AvaTaxAddress
 
 @property AvaTaxAddressType AddressType;    // Enumeration {F,G,H,P,R,S}
 @property NSString* County;
 @property NSString* FipsCode;
-@property AvaTaxCarrierRoute CarrierRoute;   // Enumeration {B,C,G,H,R}
+@property NSString* CarrierRoute;   // starts with B,C,G,H,R
 @property NSString* TaxRegionId;
 @property NSString* PostNet;
 
--- AvaTax-Calc/AvaTax-Calc-SDK/AvaTaxValidatedAddress.m
+++ AvaTax-Calc/AvaTax-Calc-SDK/AvaTaxValidatedAddress.m
@@ -53,41 +53,5 @@
     }
 }
 
-- (void)setCarrierRouteWithNSString:(NSString*)carrierRouteString {
-    if ([carrierRouteString isEqualToString:@"B"]) {
-        self.CarrierRoute = CarrierRoute_B;
-    } else if ([carrierRouteString isEqualToString:@"C"]) {
-        self.CarrierRoute = CarrierRoute_C;
-    } else if ([carrierRouteString isEqualToString:@"G"]) {
-        self.CarrierRoute = CarrierRoute_G;
-    } else if ([carrierRouteString isEqualToString:@"H"]) {
-        self.CarrierRoute = CarrierRoute_H;
-    } else if ([carrierRouteString isEqualToString:@"R"]) {
-        self.CarrierRoute = CarrierRoute_R;
-    }
-}
-
-- (id)JSONObjectForCarrierRoute {
-    switch (self.CarrierRoute) {
-        case CarrierRoute_B:
-            return @"B";
-            break;
-        case CarrierRoute_C:
-            return @"C";
-            break;
-        case CarrierRoute_G:
-            return @"G";
-            break;
-        case CarrierRoute_H:
-            return @"H";
-            break;
-        case CarrierRoute_R:
-            return @"R";
-            break;
-        default:
-            return nil;
-            break;
-    }
-}
 
 @end
