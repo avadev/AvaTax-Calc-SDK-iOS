3ffd695c736bcc8cca714d60a5bb2ddf50e80522
Rob Busack <rob.busack@gmail.com>
2014-02-27 14:23:01 -0800

    To reduce the proliferation of import statements, you can now import a single header instead of the usual dozen.

---
+++ AvaTax-Calc/AvaTax-Calc-SDK/AvaTaxHeaders.h
@@ -0,0 +1,24 @@
+//
+//  AvaTaxHeaders.h
+//  AvaTax-Calc
+//
+//  Created by Rob Busack on 2/27/14.
+//  Copyright (c) 2014 Avalara. All rights reserved.
+//
+
+#ifndef AvaTax_Calc_AvaTaxHeaders_h
+#define AvaTax_Calc_AvaTaxHeaders_h
+
+#import "AvaTaxCalc.h"
+#import "AvaTaxAddress.h"
+#import "AvaTaxValidateAddressResponse.h"
+#import "AvaTaxValidatedAddress.h"
+#import "AvaTaxEstimateTaxResponse.h"
+#import "AvaTaxGetTaxRequest.h"
+#import "AvaTaxGetTaxResponse.h"
+#import "AvaTaxCancelTaxRequest.h"
+#import "AvaTaxCancelTaxResponse.h"
+#import "AvaTaxTaxSummaryRequest.h"
+#import "AvaTaxTaxSummaryResponse.h"
+
+#endif
